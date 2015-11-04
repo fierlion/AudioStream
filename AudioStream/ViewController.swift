7f746c882a4c91eaf92d682bf9f020022156ce8b
fierlion <fierlion@gmail.com>
2015-11-04 12:00:48 -0800

    Initial Commit

---
+++ AudioStream/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AudioStream
+//
+//  Created by Ray Foote on 11/4/15.
+//  Copyright © 2015 Ray Foote. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
